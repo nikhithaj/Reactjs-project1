af4a322b92becddf72ae0b3343e73335398ed34d
nikhithaj <71509209+nikhithaj@users.noreply.github.com>
2021-04-17 22:31:02 +0530

    drum

---
+++ M-6 [drum-1]/drums.js
@@ -0,0 +1,40 @@
+
+function instrumentClick1() {
+    var mysong1 = new Audio("snare.mp3");
+    mysong1.play();
+}
+
+function instrumentClick2(){
+    var mysong2 = new Audio("tom-1.mp3");
+    mysong2.play();
+}
+
+function instrumentClick3(){
+    var mysong3 = new Audio("tom-2.mp3");
+    mysong3.play();
+}
+
+function instrumentClick4(){
+    var mysong4 = new Audio("tom-3.mp3");
+    mysong4.play();
+}
+    
+function instrumentClick5(){
+    var mysong5 = new Audio("tom-1.mp3");
+    mysong5.play();
+}
+
+function instrumentClick6(){
+    var mysong6=new Audio("snare.mp3");
+    mysong6.play();
+}
+
+function instrumentClick7(){
+    var mysong6=new Audio("snare.mp3");
+    mysong6.play();
+}
+
+function instrumentClick8(){
+    var mysong6=new Audio("tom-1.mp3");
+    mysong6.play();
+}
